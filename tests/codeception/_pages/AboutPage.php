b229c804e870e176631371c7e9bdc8199750dee0
Lancer-Serega <lancer-serega@mail.ru>
2016-09-30 02:28:33 +0300

    Start IShop Project

---
+++ tests/codeception/_pages/AboutPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\codeception\_pages;
+
+use yii\codeception\BasePage;
+
+
+/**
+ * Represents about page
+ *
+ * @property \AcceptanceTester|\FunctionalTester $actor
+ */
+class AboutPage extends BasePage
+{
+    public $route = 'site/about';
+}
